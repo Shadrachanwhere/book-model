48c4ffc433b80dee180b79e40e131173e753805c
Shad <anhweres39@gmail.com>
2026-02-25 09:05:29 +0000

    edit and delete functionality added

--- app.py
+++ app.py
@@ -24,15 +24,30 @@ def menu():
                   \r A number from 1 - 5.
                   \rPress enter to try again.
                   ''')
-        
-#cleaned id
-def clean_id(id_str, options):
+def submenu():
+     while True:
+        print(''' 
+             \r1. Edit
+              \r2. Delete
+              \r3. Return to main menu''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3']:
+            return choice
+        else:
+            input('''
+                  \rPlease choose one of the options above
+                  \r A number from 1 - 3.
+                  \r Press enter to try again.
+                  ''')
+ 
+#cleaned_id:
+def clean_id(id_str,options):
     try:
         book_id = int(id_str)
     except ValueError:
         input(''' 
-              \n*** ID ERROR ***
-              \r The id should be a number
+              \n ***** ID ERROR *****
+              \r The ID should be a number and within the specified range
               \r Press enter to try again
               ''')
         return
@@ -40,11 +55,14 @@ def clean_id(id_str, options):
         if book_id in options:
             return book_id
         else:
-            print('''
-                  \n ****** ID ERROR *******
-                  \nPlease enter a book id within the specified range''')
+            input(''' 
+                  \n ID error
+                  \n Press enter to try again
+                  ''')
             time.sleep(1.5)
             return
+    
+
 
 
 #cleaned_price
@@ -60,7 +78,7 @@ def clean_price(price_str):
         return int(price_float * 100)
     
 
-# Cleaned_date
+# cleaned_date
 def clean_date(date_str):
      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
      split_date  = date_str.split(' ')
@@ -78,6 +96,28 @@ def clean_date(date_str):
         return
      else:
          return return_date     
+
+def edit_check(column_name, current_value):
+    print(f'\n**** EDIT {column_name} ****')
+    if column_name == 'Price':
+        print(f'\rCurrent Value: {current_value/100}')
+    elif column_name =='Date':
+        print(f'\rCurrent_Value : {current_value.strftime("%B %d, %Y")}')
+    else:
+        print(f'\rCurrent Value: {current_value}') 
+    if column_name == 'Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to? ')
+            if column_name == 'Date':
+                changes = clean_date(changes)
+                if type(changes) == datetime.date:
+                    return changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+    else:
+        return input('What would you like to change the value to? ')
 
     
 
@@ -151,7 +191,23 @@ def app():
                   \n{the_book.title} by {the_book.author}
                   \rPublished Date : {the_book.published_date}
                   \rPrice : ${the_book.price / 100}\n''')
-            input('\rPress enter to return to the main menu')
+            sub_choice = submenu()
+            if sub_choice == '1':
+                the_book.title = edit_check('Title', the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.published_date = edit_check('Date', the_book.published_date)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                time.sleep(1.7)
+                print('Book updated successfully!')
+            elif sub_choice == '2':
+                # delete
+                 session.delete(the_book)
+                 session.commit()
+                 time.sleep(1.6)
+                 print('Book deleted successfully!')
+                
+           
         elif choice == '4':
             # book analysis
             pass
